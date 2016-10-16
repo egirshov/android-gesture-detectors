f03daac81ff49552aa78b4ae17e8a24bb9ef7b79
Eugene Girshov <eugene@solu.co>
2016-10-16 19:49:45 +0300

    use Math instead of android.util.FloatMath
    FloatMath has been deprecated in API 22 and removed in API version 23

--- library/src/main/java/com/almeros/android/multitouch/TwoFingerGestureDetector.java
+++ library/src/main/java/com/almeros/android/multitouch/TwoFingerGestureDetector.java
@@ -2,7 +2,6 @@ package com.almeros.android.multitouch;
 
 import android.content.Context;
 import android.util.DisplayMetrics;
-import android.util.FloatMath;
 import android.view.MotionEvent;
 import android.view.ViewConfiguration;
 
@@ -15,15 +14,15 @@ import android.view.ViewConfiguration;
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
  *
  *  Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
- *  Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer 
+ *  Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer
  *  in the documentation and/or other materials provided with the distribution.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
- * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, 
- * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
- * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
  * OF SUCH DAMAGE.
  */
 public abstract class TwoFingerGestureDetector extends BaseGestureDetector {
@@ -31,33 +30,33 @@ public abstract class TwoFingerGestureDetector extends BaseGestureDetector {
     private final float mEdgeSlop;
     private float mRightSlopEdge;
     private float mBottomSlopEdge;
-    
+
 	protected float mPrevFingerDiffX;
 	protected float mPrevFingerDiffY;
 	protected float mCurrFingerDiffX;
 	protected float mCurrFingerDiffY;
-    
+
     private float mCurrLen;
     private float mPrevLen;
-	
+
     public TwoFingerGestureDetector(Context context) {
     	super(context);
-    	
+
         ViewConfiguration config = ViewConfiguration.get(context);
-        mEdgeSlop = config.getScaledEdgeSlop();       	
+        mEdgeSlop = config.getScaledEdgeSlop();
     }
-    
+
 	@Override
 	protected abstract void handleStartProgressEvent(int actionCode, MotionEvent event);
 
 	@Override
 	protected abstract void handleInProgressEvent(int actionCode, MotionEvent event);
-	
+
 	protected void updateStateByEvent(MotionEvent curr){
 		super.updateStateByEvent(curr);
-		
+
 		final MotionEvent prev = mPrevEvent;
-		
+
         mCurrLen = -1;
         mPrevLen = -1;
 
@@ -70,7 +69,7 @@ public abstract class TwoFingerGestureDetector extends BaseGestureDetector {
         final float pvy = py1 - py0;
         mPrevFingerDiffX = pvx;
         mPrevFingerDiffY = pvy;
-        
+
         // Current
         final float cx0 = curr.getX(0);
         final float cy0 = curr.getY(0);
@@ -81,18 +80,18 @@ public abstract class TwoFingerGestureDetector extends BaseGestureDetector {
         mCurrFingerDiffX = cvx;
         mCurrFingerDiffY = cvy;
 	}
-	
+
     /**
      * Return the current distance between the two pointers forming the
      * gesture in progress.
-     * 
+     *
      * @return Distance between pointers in pixels.
      */
     public float getCurrentSpan() {
         if (mCurrLen == -1) {
             final float cvx = mCurrFingerDiffX;
             final float cvy = mCurrFingerDiffY;
-            mCurrLen = FloatMath.sqrt(cvx*cvx + cvy*cvy);
+            mCurrLen = (float)Math.sqrt(cvx*cvx + cvy*cvy);
         }
         return mCurrLen;
     }
@@ -100,20 +99,20 @@ public abstract class TwoFingerGestureDetector extends BaseGestureDetector {
     /**
      * Return the previous distance between the two pointers forming the
      * gesture in progress.
-     * 
+     *
      * @return Previous distance between pointers in pixels.
      */
     public float getPreviousSpan() {
         if (mPrevLen == -1) {
             final float pvx = mPrevFingerDiffX;
             final float pvy = mPrevFingerDiffY;
-            mPrevLen = FloatMath.sqrt(pvx*pvx + pvy*pvy);
+            mPrevLen = (float)Math.sqrt(pvx*pvx + pvy*pvy);
         }
         return mPrevLen;
     }
-    
+
     /**
-     * MotionEvent has no getRawX(int) method; simulate it pending future API approval. 
+     * MotionEvent has no getRawX(int) method; simulate it pending future API approval.
      * @param event
      * @param pointerIndex
      * @return
@@ -122,12 +121,12 @@ public abstract class TwoFingerGestureDetector extends BaseGestureDetector {
         float offset = event.getX() - event.getRawX();
         if(pointerIndex < event.getPointerCount()){
         	return event.getX(pointerIndex) + offset;
-        } 
+        }
         return 0f;
     }
 
     /**
-     * MotionEvent has no getRawY(int) method; simulate it pending future API approval. 
+     * MotionEvent has no getRawY(int) method; simulate it pending future API approval.
      * @param event
      * @param pointerIndex
      * @return
@@ -136,14 +135,14 @@ public abstract class TwoFingerGestureDetector extends BaseGestureDetector {
         float offset = event.getY() - event.getRawY();
         if(pointerIndex < event.getPointerCount()){
         	return event.getY(pointerIndex) + offset;
-        } 
+        }
         return 0f;
     }
 
 	/**
 	 * Check if we have a sloppy gesture. Sloppy gestures can happen if the edge
 	 * of the user's hand is touching the screen, for example.
-	 * 
+	 *
 	 * @param event
 	 * @return
 	 */
@@ -152,11 +151,11 @@ public abstract class TwoFingerGestureDetector extends BaseGestureDetector {
         DisplayMetrics metrics = mContext.getResources().getDisplayMetrics();
         mRightSlopEdge = metrics.widthPixels - mEdgeSlop;
         mBottomSlopEdge = metrics.heightPixels - mEdgeSlop;
-        
+
         final float edgeSlop = mEdgeSlop;
         final float rightSlop = mRightSlopEdge;
         final float bottomSlop = mBottomSlopEdge;
-        
+
         final float x0 = event.getRawX();
         final float y0 = event.getRawY();
         final float x1 = getRawX(event, 1);
@@ -173,8 +172,8 @@ public abstract class TwoFingerGestureDetector extends BaseGestureDetector {
         	 return true;
         } else if (p1sloppy) {
         	 return true;
-        } 
+        }
         return false;
-    }    
+    }
 
 }
